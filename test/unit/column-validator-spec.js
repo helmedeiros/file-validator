d22852b00d13d854250c4eb87cf2b30dd01ee7e0
helmedeiros <helio.cabralmedeiros@gmail.com>
2016-07-16 20:18:56 -0300

    remove unecessary files

--- test/unit/column-validator-spec.js
+++
@@ -1,7 +0,0 @@
-describe("CustomerExperience", function() {
-    describe('resourceService', function() {
-        it('should be able to invoke get resource', function() {
-            expect(0).toEqual(0);
-        });
-    });
-});
